9ae07df1524e4af7e1832006f6dd28f86db947cc
Kazunori Jo <itochan315@gmail.com>
2017-05-07 21:54:43 +0900

    Use go-flags and add help

--- ptrhost.go
+++ ptrhost.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	"flag"
 	"net"
+	"os"
+
+	flags "github.com/jessevdk/go-flags"
 )
 
 func main() {
-	flag.Parse()
-	hostname := flag.Arg(0)
+	parser := flags.NewParser(nil, flags.Default)
+	parser.Usage = "HOSTNAME [OPTIONS]"
+	args, _ := parser.Parse()
 
+	if len(args) == 0 {
+		os.Exit(1)
+	}
+
+	hostname := args[0]
 	addr, _ := net.LookupHost(hostname)
 	for _, v := range addr {
 		ptrAddr, _ := net.LookupAddr(v)
