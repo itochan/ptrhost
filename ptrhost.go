b6de2907a0a684d2ff3000c89b51e7c4044c9a41
Kazunori Jo <itochan315@gmail.com>
2017-05-08 14:06:45 +0900

    Bump version to 1.0.1

--- ptrhost.go
+++ ptrhost.go
@@ -8,7 +8,7 @@ import (
 	flags "github.com/jessevdk/go-flags"
 )
 
-const version = "1.0.0"
+const version = "1.0.1"
 
 var opts struct {
 	Version bool `short:"v" long:"version" description:"Show version"`
